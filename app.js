4ca41e1f2ebb9e816abdc0fc9380d872a92cfae8
Heet Kalaria <heetkalaria@yahoo.com>
2020-06-19 17:47:02 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const container=document.getElementById('container');
+const text=document.getElementById('text');
+
+const totalTime =7500;
+const breatheTime = (totalTime/5)*2;
+const holdTime = totalTime/5;
+
+breathAnimation();
+function breathAnimation(){
+    text.innerText = "Breathe In!";
+    setTimeout(()=>{
+        text.innerHTML="Hold";
+
+        setTimeout(()=>{
+            text.innerHTML="Breathe Out!";
+        },holdTime);
+    },breatheTime);
+}
+
+setInterval(breathAnimation, totalTime);
